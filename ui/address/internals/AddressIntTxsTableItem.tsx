b054b4af30b5a6e4cbc16384cf0e8438154f743c
Pouya <pouya@kenshi.io>
2024-05-29 22:58:56 +0200

    Add Ozone Mining label types

--- ui/address/internals/AddressIntTxsTableItem.tsx
+++ ui/address/internals/AddressIntTxsTableItem.tsx
@@ -1,19 +1,42 @@
-import { Tr, Td, Box, Flex, Skeleton } from '@chakra-ui/react';
-import BigNumber from 'bignumber.js';
-import React from 'react';
+import { Tr, Td, Box, Flex, Skeleton } from "@chakra-ui/react";
+import BigNumber from "bignumber.js";
+import React from "react";
 
-import type { InternalTransaction } from 'types/api/internalTransaction';
+import type { InternalTransaction } from "types/api/internalTransaction";
 
-import config from 'configs/app';
-import useTimeAgoIncrement from 'lib/hooks/useTimeAgoIncrement';
-import AddressFromTo from 'ui/shared/address/AddressFromTo';
-import Tag from 'ui/shared/chakra/Tag';
-import BlockEntity from 'ui/shared/entities/block/BlockEntity';
-import TxEntity from 'ui/shared/entities/tx/TxEntity';
-import TxStatus from 'ui/shared/statusTag/TxStatus';
-import { TX_INTERNALS_ITEMS } from 'ui/tx/internals/utils';
+import config from "configs/app";
+import useTimeAgoIncrement from "lib/hooks/useTimeAgoIncrement";
+import AddressFromTo from "ui/shared/address/AddressFromTo";
+import Tag from "ui/shared/chakra/Tag";
+import BlockEntity from "ui/shared/entities/block/BlockEntity";
+import TxEntity from "ui/shared/entities/tx/TxEntity";
+import TxStatus from "ui/shared/statusTag/TxStatus";
+import { TX_INTERNALS_ITEMS } from "ui/tx/internals/utils";
+import { AddressParam } from "types/api/addressParams";
 
-type Props = InternalTransaction & { currentAddress: string; isLoading?: boolean }
+type Props = InternalTransaction & {
+  currentAddress: string;
+  isLoading?: boolean;
+};
+
+// TODO: Move these to a config file
+
+const EVM_WRITER_ADDRESS = "0xd100ec0000000000000000000000000000000000";
+const NATIVE_MINTER_ADDRESS = "0x6d696e7400000000000000000000000000000000";
+
+const getTypeTitle = (
+  from: AddressParam,
+  to: AddressParam | null,
+  type: string
+) => {
+  if (
+    from.hash.toLowerCase() === NATIVE_MINTER_ADDRESS &&
+    to?.hash.toLowerCase() === EVM_WRITER_ADDRESS
+  ) {
+    return "Mining";
+  }
+  return TX_INTERNALS_ITEMS.find(({ id }) => id === type)?.title;
+};
 
 const AddressIntTxsTableItem = ({
   type,
@@ -29,7 +52,7 @@ const AddressIntTxsTableItem = ({
   currentAddress,
   isLoading,
 }: Props) => {
-  const typeTitle = TX_INTERNALS_ITEMS.find(({ id }) => id === type)?.title;
+  const typeTitle = getTypeTitle(from, to, type);
   const toData = to ? to : createdContract;
 
   const timeAgo = useTimeAgoIncrement(timestamp, true);
@@ -37,52 +60,65 @@ const AddressIntTxsTableItem = ({
   return (
     <Tr alignItems="top">
       <Td verticalAlign="middle">
-        <Flex rowGap={ 3 } flexWrap="wrap">
+        <Flex rowGap={3} flexWrap="wrap">
           <TxEntity
-            hash={ txnHash }
-            isLoading={ isLoading }
-            fontWeight={ 700 }
+            hash={txnHash}
+            isLoading={isLoading}
+            fontWeight={700}
             noIcon
             truncation="constant_long"
           />
-          { timestamp && (
-            <Skeleton isLoaded={ !isLoading } color="text_secondary" fontWeight="400" fontSize="sm">
-              <span>{ timeAgo }</span>
+          {timestamp && (
+            <Skeleton
+              isLoaded={!isLoading}
+              color="text_secondary"
+              fontWeight="400"
+              fontSize="sm"
+            >
+              <span>{timeAgo}</span>
             </Skeleton>
-          ) }
+          )}
         </Flex>
       </Td>
       <Td verticalAlign="middle">
-        <Flex rowGap={ 2 } flexWrap="wrap">
-          { typeTitle && (
+        <Flex rowGap={2} flexWrap="wrap">
+          {typeTitle && (
             <Box w="126px" display="inline-block">
-              <Tag colorScheme="cyan" mr={ 5 } isLoading={ isLoading }>{ typeTitle }</Tag>
+              <Tag colorScheme="cyan" mr={5} isLoading={isLoading}>
+                {typeTitle}
+              </Tag>
             </Box>
-          ) }
-          <TxStatus status={ success ? 'ok' : 'error' } errorText={ error } isLoading={ isLoading }/>
+          )}
+          <TxStatus
+            status={success ? "ok" : "error"}
+            errorText={error}
+            isLoading={isLoading}
+          />
         </Flex>
       </Td>
       <Td verticalAlign="middle">
         <BlockEntity
-          isLoading={ isLoading }
-          number={ block }
+          isLoading={isLoading}
+          number={block}
           noIcon
           fontSize="sm"
-          lineHeight={ 5 }
-          fontWeight={ 500 }
+          lineHeight={5}
+          fontWeight={500}
         />
       </Td>
       <Td verticalAlign="middle">
         <AddressFromTo
-          from={ from }
-          to={ toData }
-          current={ currentAddress }
-          isLoading={ isLoading }
+          from={from}
+          to={toData}
+          current={currentAddress}
+          isLoading={isLoading}
         />
       </Td>
       <Td isNumeric verticalAlign="middle">
-        <Skeleton isLoaded={ !isLoading } display="inline-block" minW={ 6 }>
-          { BigNumber(value).div(BigNumber(10 ** config.chain.currency.decimals)).toFormat() }
+        <Skeleton isLoaded={!isLoading} display="inline-block" minW={6}>
+          {BigNumber(value)
+            .div(BigNumber(10 ** config.chain.currency.decimals))
+            .toFormat()}
         </Skeleton>
       </Td>
     </Tr>
