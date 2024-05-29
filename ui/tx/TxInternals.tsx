4f1faadb55ead90827895bfc0b336db5e5d69537
Pouya <pouya@kenshi.io>
2024-05-29 23:27:38 +0200

    Add Ozone Mining label types

--- ui/tx/TxInternals.tsx
+++ ui/tx/TxInternals.tsx
@@ -1,7 +1,7 @@
-import { Show, Hide } from '@chakra-ui/react';
-import React from 'react';
+import { Show, Hide } from "@chakra-ui/react";
+import React from "react";
 
-import type { InternalTransaction } from 'types/api/internalTransaction';
+import type { InternalTransaction } from "types/api/internalTransaction";
 
 import compareBns from 'lib/bigint/compareBns';
 // import { apos } from 'lib/html-entities';
@@ -11,23 +11,26 @@ import ActionBar, { ACTION_BAR_HEIGHT_DESKTOP } from 'ui/shared/ActionBar';
 import DataListDisplay from 'ui/shared/DataListDisplay';
 // import FilterInput from 'ui/shared/filters/FilterInput';
 // import TxInternalsFilter from 'ui/tx/internals/TxInternalsFilter';
-import Pagination from 'ui/shared/pagination/Pagination';
-import useQueryWithPages from 'ui/shared/pagination/useQueryWithPages';
-import { default as getNextSortValueShared } from 'ui/shared/sort/getNextSortValue';
-import TxInternalsList from 'ui/tx/internals/TxInternalsList';
-import TxInternalsTable from 'ui/tx/internals/TxInternalsTable';
-import type { Sort, SortField } from 'ui/tx/internals/utils';
-import TxPendingAlert from 'ui/tx/TxPendingAlert';
-import TxSocketAlert from 'ui/tx/TxSocketAlert';
+import Pagination from "ui/shared/pagination/Pagination";
+import useQueryWithPages from "ui/shared/pagination/useQueryWithPages";
+import { default as getNextSortValueShared } from "ui/shared/sort/getNextSortValue";
+import TxInternalsList from "ui/tx/internals/TxInternalsList";
+import TxInternalsTable from "ui/tx/internals/TxInternalsTable";
+import type { Sort, SortField } from "ui/tx/internals/utils";
+import TxPendingAlert from "ui/tx/TxPendingAlert";
+import TxSocketAlert from "ui/tx/TxSocketAlert";
 
-import type { TxQuery } from './useTxQuery';
+import type { TxQuery } from "./useTxQuery";
 
 const SORT_SEQUENCE: Record<SortField, Array<Sort | undefined>> = {
-  value: [ 'value-desc', 'value-asc', undefined ],
-  'gas-limit': [ 'gas-limit-desc', 'gas-limit-asc', undefined ],
+  value: ["value-desc", "value-asc", undefined],
+  "gas-limit": ["gas-limit-desc", "gas-limit-asc", undefined],
 };
 
-const getNextSortValue = (getNextSortValueShared<SortField, Sort>).bind(undefined, SORT_SEQUENCE);
+const getNextSortValue = getNextSortValueShared<SortField, Sort>.bind(
+  undefined,
+  SORT_SEQUENCE
+);
 
 const sortFn = (sort: Sort | undefined) => (a: InternalTransaction, b: InternalTransaction) => {
   switch (sort) {
@@ -68,13 +71,18 @@ const TxInternals = ({ txQuery }: Props) => {
   // filters are not implemented yet in api
   // const [ filters, setFilters ] = React.useState<Array<TxInternalsType>>([]);
   // const [ searchTerm, setSearchTerm ] = React.useState<string>('');
-  const [ sort, setSort ] = React.useState<Sort>();
+  const [sort, setSort] = React.useState<Sort>();
   const { data, isPlaceholderData, isError, pagination } = useQueryWithPages({
-    resourceName: 'tx_internal_txs',
+    resourceName: "tx_internal_txs",
     pathParams: { hash: txQuery.data?.hash },
     options: {
-      enabled: !txQuery.isPlaceholderData && Boolean(txQuery.data?.hash) && Boolean(txQuery.data?.status),
-      placeholderData: generateListStub<'tx_internal_txs'>(INTERNAL_TX, 3, { next_page_params: null }),
+      enabled:
+        !txQuery.isPlaceholderData &&
+        Boolean(txQuery.data?.hash) &&
+        Boolean(txQuery.data?.status),
+      placeholderData: generateListStub<"tx_internal_txs">(INTERNAL_TX, 3, {
+        next_page_params: null,
+      }),
     },
   });
 
@@ -82,29 +90,38 @@ const TxInternals = ({ txQuery }: Props) => {
   //   setFilters(nextValue);
   // }, []);
 
-  const handleSortToggle = React.useCallback((field: SortField) => {
-    return () => {
-      if (isPlaceholderData) {
-        return;
-      }
-      setSort(getNextSortValue(field));
-    };
-  }, [ isPlaceholderData ]);
+  const handleSortToggle = React.useCallback(
+    (field: SortField) => {
+      return () => {
+        if (isPlaceholderData) {
+          return;
+        }
+        setSort(getNextSortValue(field));
+      };
+    },
+    [isPlaceholderData]
+  );
 
   if (!txQuery.isPlaceholderData && !txQuery.isError && !txQuery.data?.status) {
-    return txQuery.socketStatus ? <TxSocketAlert status={ txQuery.socketStatus }/> : <TxPendingAlert/>;
+    return txQuery.socketStatus ? (
+      <TxSocketAlert status={txQuery.socketStatus} />
+    ) : (
+      <TxPendingAlert />
+    );
   }
 
   const filteredData = data?.items
     .slice()
-  // .filter(({ type }) => filters.length > 0 ? filters.includes(type) : true)
-  // .filter(searchFn(searchTerm))
+    // .filter(({ type }) => filters.length > 0 ? filters.includes(type) : true)
+    // .filter(searchFn(searchTerm))
     .sort(sortFn(sort));
 
   const content = filteredData ? (
     <>
-      <Show below="lg" ssr={ false }><TxInternalsList data={ filteredData } isLoading={ isPlaceholderData }/></Show>
-      <Hide below="lg" ssr={ false }>
+      <Show below="lg" ssr={false}>
+        <TxInternalsList data={filteredData} isLoading={isPlaceholderData} />
+      </Show>
+      <Hide below="lg" ssr={false}>
         <TxInternalsTable
           data={ filteredData }
           sort={ sort }
@@ -117,24 +134,24 @@ const TxInternals = ({ txQuery }: Props) => {
   ) : null;
 
   const actionBar = pagination.isVisible ? (
-    <ActionBar mt={ -6 }>
-      { /* <TxInternalsFilter onFilterChange={ handleFilterChange } defaultFilters={ filters } appliedFiltersNum={ filters.length }/> */ }
-      { /* <FilterInput onChange={ setSearchTerm } maxW="360px" ml={ 3 } size="xs" placeholder="Search by addresses, hash, method..."/> */ }
-      <Pagination ml="auto" { ...pagination }/>
+    <ActionBar mt={-6}>
+      {/* <TxInternalsFilter onFilterChange={ handleFilterChange } defaultFilters={ filters } appliedFiltersNum={ filters.length }/> */}
+      {/* <FilterInput onChange={ setSearchTerm } maxW="360px" ml={ 3 } size="xs" placeholder="Search by addresses, hash, method..."/> */}
+      <Pagination ml="auto" {...pagination} />
     </ActionBar>
   ) : null;
 
   return (
     <DataListDisplay
-      isError={ isError || txQuery.isError }
-      items={ data?.items }
+      isError={isError || txQuery.isError}
+      items={data?.items}
       emptyText="There are no internal transactions for this transaction."
       // filterProps={{
       // emptyFilteredText: `Couldn${ apos }t find any transaction that matches your query.`.
       // hasActiveFilters: Boolean(filters.length || searchTerm),
       // }}
-      content={ content }
-      actionBar={ actionBar }
+      content={content}
+      actionBar={actionBar}
     />
   );
 };
